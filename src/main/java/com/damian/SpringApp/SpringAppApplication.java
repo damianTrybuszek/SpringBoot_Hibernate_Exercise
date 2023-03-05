679e166d0bf6e0a7360c4950bb8fce8b3833963b
damianTrybuszek <damian.trybuszek@gmail.com>
2023-03-05 22:55:20 +0100

    Application properties created.

--- src/main/java/com/damian/SpringApp/SpringAppApplication.java
+++ src/main/java/com/damian/SpringApp/SpringAppApplication.java
@@ -2,12 +2,14 @@ package com.damian.SpringApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SpringAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAppApplication.class, args);
+		System.out.println("Hello Damian!");
 	}
 
 }
